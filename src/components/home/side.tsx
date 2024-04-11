77eed30e1bc47905c88c558cca5f6b1d995e6ff2
Felipe Batista <hellojavaa@gmail.com>
2024-04-11 00:42:31 -0300

    refactor: change colors in hero right side

--- src/components/home/side.tsx
+++ src/components/home/side.tsx
@@ -12,7 +12,7 @@ export function HomeRightSide() {
       className="flex-col gap-[2vh] md:justify-end md:w-[70%] mt-[2vh] ml-auto hidden md:flex "
     >
       <div className="flex flex-col gap-[1vh] pb-[2vh] border-b border-white border-opacity-30">
-        <h2 className="font-semibold text-title-sm">SOBRE MI </h2>
+        <h2 className="font-semibold text-title-sm text-primary">SOBRE MI </h2>
         <p className="text-description-sm">
           Me encanta enfrentar desafíos técnicos y encontrar soluciones
           innovadoras para optimizar el funcionamiento de las aplicaciones.
@@ -22,7 +22,7 @@ export function HomeRightSide() {
       </div>
 
       <div className="flex flex-col gap-2 pb-6 border-b border-white border-opacity-30">
-        <h2 className="font-semibold text-title-sm">PROYECTOS</h2>
+        <h2 className="font-semibold text-title-sm text-primary">PROYECTOS</h2>
         <p className="text-description-sm">
           Estos son algunos de los proyectos en los que he trabajado. Si desea
           ver más y sus procesos, puede consultar mi perfil de Github.
