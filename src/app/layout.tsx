edd93e09e28bfe5c33b371c5fd8d0ca4804f13ab
Felipe Batista <hellojavaa@gmail.com>
2025-09-01 10:05:45 -0300

    Revert "remove plausible"
    This reverts commit cf8b2e91937c949275bf1292242cef14aba52069.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
+      <head>
+        <script
+          defer
+          data-domain="felibatista.live"
+          src="https://plausible.io/js/plausible.js"
+        ></script>
+      </head>
       <body
         className={cn(inter.className, "text-black bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]")}
       >
